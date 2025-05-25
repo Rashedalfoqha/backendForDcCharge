dcafea756ce2e0cc0cad3ebef35e66a4955a2fc4
Rashedalfoqha <rashedmohammadalfoqha@gmail.com>
2025-05-25 09:39:05 +0300

    error detils

--- models/dataBase.js
+++ models/dataBase.js
@@ -1,25 +1,7 @@
 const mongoose = require("mongoose");
-
 mongoose
-  .connect(process.env.DB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.DB_URL)
   .then(() => {
     console.log("DB Ready To Use");
   })
-  .catch((err) => {
-    console.error("DB Connection Error:", err);
-  });
-
-mongoose.connection.on("connected", () => {
-  console.log("Mongoose connected to DB");
-});
-
-mongoose.connection.on("error", (err) => {
-  console.error("Mongoose connection error:", err);
-});
-
-mongoose.connection.on("disconnected", () => {
-  console.log("Mongoose disconnected from DB");
-});
+  .catch(() => {});
