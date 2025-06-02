76ee54ca19d8adad431179c5ec11ee5e3b914ed9
Rashedalfoqha <rashedmohammadalfoqha@gmail.com>
2025-06-02 15:48:36 +0300

    edit pagecontent

--- models/pageContent.js
+++ models/pageContent.js
@@ -1,19 +1,27 @@
 const mongoose = require('mongoose');
 
+const featureSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  value: { type: String, required: true }
+}, { _id: false });
+
 const sectionSchema = new mongoose.Schema({
   id: { type: String, required: true },
   heading: String,
   content: mongoose.Schema.Types.Mixed,
   image: mongoose.Schema.Types.Mixed,
   ctaText: String,
-  ctaLink: String
-}, { _id: false }); 
+  ctaLink: String,
+features: {
+  type: [featureSchema],
+  default: []
+}}, { _id: false }); 
 
 const pageContentSchema = new mongoose.Schema({
   page: { type: String, required: true },
   language: { type: String, enum: ['en', 'ar'], required: true },
   title: String,
-  sections: [sectionSchema], // ✅ define sections properly here
+  sections: [sectionSchema],
   lastUpdated: { type: Date, default: Date.now }
 }, { timestamps: true });
 
